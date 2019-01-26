b72ce4df4d7f500d75337d0030e272627e2b24e2
Klaus Kazlauskas <klausmachado@gmail.com>
2019-01-26 17:58:28 -0200

    chore(ng-environmenter): remove unused files

--- projects/ng-environmenter/src/public_api.ts
+++ projects/ng-environmenter/src/public_api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ng-environmenter
  */
-
-export * from './lib/ng-environmenter.service';
-export * from './lib/ng-environmenter.component';
-export * from './lib/ng-environmenter.module';
